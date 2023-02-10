57f91a98a998cf57c07288f6ebbf2f79af3a5ff2
RadhitViBritannia <110009337+RadhitViBritannia@users.noreply.github.com>
2023-02-10 09:10:29 +0700

    Add files via upload

---
+++ suisei/registrasi.php
@@ -0,0 +1,57 @@
+<div class="container mt-3">
+<div class="col-md-4 offset-md-4">
+<div class="card">
+    <div class="card-header">
+        REGISTRASI
+    </div>
+<div class="card-body">
+<form action="" method="POST">
+    <div class="mb-3">
+        <label class="form-label">Nik</label>
+        <input type="number" class="form-control" name="nik" placeholder="Nik" required>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Nama Lengkap</label>
+        <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap" required>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Username</label>
+        <input type="text" class="form-control" name="username" placeholder="Username" required>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" class="form-control" name="password" placeholder="Password" required>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">No. Telp</label>
+        <input type="number" class="form-control" name="telp" placeholder="No Telp" required>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Daftar</button>
+    <a href="index.php?page=login" class="m-3">Sudah punya akun? Login disini!</a>
+
+    </div>
+    <div class="card-footer"></div>
+    </form>
+</div>
+</div>
+</div>
+
+<?php
+include 'config/koneksi.php';
+if (isset($_POST['kirim'])) {
+    $nik = $_POST['nik'];
+    $nama = $_POST['nama'];
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $telp = $_POST['telp'];
+    $level = 'masyarakat';
+
+    $query = mysqli_query($koneksi, "INSERT INTO masyarakat VALUES ('$nik','$nama','$username','$password','$telp','$level')");
+
+    if ($query) {
+        header('location:index.php?page=login');
+    }
+}
+
+?>
