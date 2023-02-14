0f091fe39977686d870fc457886e7e3607b889d1
RadhitViBritannia <110009337+RadhitViBritannia@users.noreply.github.com>
2023-02-14 11:54:52 +0700

    Add files via upload

---
+++ suisei4/masyarakat/dashboard.php
@@ -0,0 +1,15 @@
+<div class="card">
+<div class="card-body">
+<div class="row">
+        <div class="col-lg-6 p-4">
+            <h4>Selamat datang <?php echo $user['nama'] ?></h4>
+            <p class="text-secondary">
+                Berikan pengaduan anda di aplikasi pengaduan masyarakat
+            <p>
+            <a href="" class="btn btn-primary">Beri Pengaduan</a>
+</div>
+<div class="col-lg-6 text-center">
+    <img src="img/main/dashboard.svg" width="200px" alt="">
+</div>
+</div>
+</div>
---
+++ suisei4/masyarakat/pengaduan.php
@@ -0,0 +1,1 @@
+<a href="?page=form-pengaduan" class="btn btn-primary">Tulis Pengaduan</a>
